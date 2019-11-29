d475e5931714f51324614cb958e2b6f24f528746
sharkyb <nichlong@pdx.edu>
2019-11-28 21:44:37 -0800

    pushing my progress on the switch for the filter

--- Prototype/Prototype_Code/PrototypeCode_NeoPixels/PrototypeCode_NeoPixels.ino
+++ Prototype/Prototype_Code/PrototypeCode_NeoPixels/PrototypeCode_NeoPixels.ino
@@ -40,6 +40,9 @@ void setup()
 // This will loop for all of time
 void loop() 
 {
+    /*
+       THIS IS OLD CODE
+
     // Reading in from the 8 band pass filters
     int pot1 = analogRead(pot1Pin);
     int pot2 = analogRead(pot2Pin);
@@ -54,25 +57,54 @@ void loop()
     char printer[99];	
     sprintf(printer, "Value 1: %d  Value2: %d  Value3: %d  Value4: %d  Value5: %d  Value6: %d  Value7: %d  Value8: %d", pot1, pot2, pot3, pot4, pot5, pot6, pot7, pot8);
     Serial.println(printer);
-//Serial.println(pot8);
+    //Serial.println(pot8);
+     */
+
     /*   Serial.print(" Value 1: ");Serial.print(pot1);Serial.print(" Value 2: ");Serial.print(pot2);Serial.print(" Value 3: ");Serial.print(pot3);Serial.print(" Value 4: ");Serial.print(pot4);
          Serial.print(" Value 5: ");Serial.print(pot5);Serial.print(" Value 6: ");Serial.print(pot6);Serial.print(" Value 7: ");Serial.print(pot7);Serial.print(" Value 8: ");Serial.println(pot8);
      */
+    for (i = 1; i < 9; i++)
+    {
+        // Sets the number of LEDS for the volume level
+        switch (i)
+        {
+            case(1):
+                pot1 = getSample();
+                setLEDcolor(1, pot1);
+            case(2):
+                pot2 = getSample();
+                setLEDcolor(2, pot2);
+            case(3):
+                pot3 = getSample();
+                setLEDcolor(3, pot3);
+            case(4):
+                pot4 = getSample();
+                setLEDcolor(4, pot4);
+            case(5):
+                pot5 = getSample():
+                setLEDcolor(5, pot5);
+            case(6):
+                pot6 = getSample();
+                setLEDcolor(6, pot6);
+            case(7):
+                pot7 = getSample();
+                setLEDcolor(7, pot7);
+            case(8):
+                pot8 = getSample();
+                setLEDcolor(8, pot8);
+        }
 
-    // Sets the number of LEDS for the volume level
-    setLEDcolor(1, pot1);
-    setLEDcolor(2, pot2);
-    setLEDcolor(3, pot3);
-    setLEDcolor(4, pot4);
-    setLEDcolor(5, pot5);
-    setLEDcolor(6, pot6);
-    setLEDcolor(7, pot7);
-    setLEDcolor(8, pot8);
+                // Turns all LEDS Off if there is "no signal"
+                for (int j = 0; j < NumLEDS; j++)
+                    leds[j] = CRGB::C0;
+                FastLED.show();
+    }
+}
 
-    // Turns all LEDS Off if there is "no signal"
-    for (int j = 0; j < NumLEDS; j++)
-        leds[j] = CRGB::C0;
-    FastLED.show();
+void getSample()
+{
+    // Will comunicate (notsure how) with the filter chip to get the value of each filter
+
 }
 
 void setLEDcolor(int pixelNum, int pot)
@@ -87,7 +119,7 @@ void setLEDcolor(int pixelNum, int pot)
     int row7 = pixelNum-1 + (8 * 6);
     int row8 = pixelNum-1 + (8 * 7);
 
-  FastLED.setBrightness(63);
+    FastLED.setBrightness(63);
     // Volume stage 1
     if (pot >15 && pot < 255)
     {
@@ -118,7 +150,7 @@ void setLEDcolor(int pixelNum, int pot)
                 leds[row1] = CRGB::C8;
                 break;
         }
-      
+
         FastLED.show();
     }
 
@@ -160,7 +192,7 @@ void setLEDcolor(int pixelNum, int pot)
                 leds[row2] = CRGB::C8;
                 break;
         }
-       FastLED.show();
+        FastLED.show();
     }
 
     // Volume stage 3
@@ -336,12 +368,12 @@ void setLEDcolor(int pixelNum, int pot)
     }
 
 
-// Volume stage 6
-else if (pot > 762 && pot < 889)
-{
-    switch (pixelNum)
+    // Volume stage 6
+    else if (pot > 762 && pot < 889)
     {
-        case 1:
+        switch (pixelNum)
+        {
+            case 1:
                 leds[row1] = CRGB::C1;
                 leds[row2] = CRGB::C1;
                 leds[row3] = CRGB::C1;
@@ -405,9 +437,9 @@ else if (pot > 762 && pot < 889)
                 leds[row5] = CRGB::C8;
                 leds[row6] = CRGB::C8;
                 break;
+        }
+        FastLED.show();
     }
-    FastLED.show();
-  }
     // Volume stage 7
     else if (pot > 889 && pot < 1000)
     {
@@ -587,6 +619,6 @@ void rowMultiplier(int multi)
     {
         String R = "row";
         R.concat(i);
-//        leds[R] = CRGB::C1;
+        //        leds[R] = CRGB::C1;
     }
 }
