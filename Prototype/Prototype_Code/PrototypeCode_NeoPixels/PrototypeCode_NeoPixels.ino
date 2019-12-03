c9921425c2340b9865f51d8baee0b1c027468112
sharkyb <nichlong@pdx.edu>
2019-12-03 12:40:47 -0800

    pushing my progress on the switch for the filter

--- Prototype/Prototype_Code/PrototypeCode_NeoPixels/PrototypeCode_NeoPixels.ino
+++ Prototype/Prototype_Code/PrototypeCode_NeoPixels/PrototypeCode_NeoPixels.ino
@@ -42,7 +42,8 @@ void setup()
 { 
     FastLED.addLeds<NEOPIXEL, DataPin>(leds, NumLEDS);
     Serial.begin(57600);
-    analogReference(INTERNAL);
+    analogReference(INTERNAL); // Sets the AREF to 2.5 for the reference for the filter chip
+    analogWrite(IO11,127); // (MAYBE?) Sets the clock signal to the IO11 Pin at 50% duty cycle
     digitalWrite(RESETPIN, OUTPUT); 
     digitalWrite(STROBEPIN, OUTPUT);
 
